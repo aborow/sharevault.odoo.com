bce9d6d9e070151a9edc3eafbef55ded65ce6b9a
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-04-18 12:29:47 +0100

    [UPD] sharevault - logs in contacts

--- sharevault/models/partner.py
+++ sharevault/models/partner.py
@@ -15,3 +15,30 @@ class Partner(models.Model):
     def get_sharevault_count(self):
         self.ensure_one()
         self.sharevault_ids_count = len(self.sharevault_ids)
+
+
+    auditlog_ids_count = fields.Integer('Auditlog count', compute='get_auditlog_count')
+
+    @api.multi
+    def get_auditlog_count(self):
+        self.ensure_one()
+        AuditLog = self.env['auditlog.log']
+        model_id = self.env['ir.model'].search([('model','=',self._name)])
+        self.auditlog_ids_count = AuditLog.search_count([
+                                                        ('model_id','=',model_id.id),
+                                                        ('res_id','=',self.id)
+                                                        ])
+
+    @api.multi
+    def call_auditlog(self):
+        self.ensure_one()
+        action = self.env.ref('auditlog.action_auditlog_log_tree').read()[0]
+        model_id = self.env['ir.model'].search([('model','=',self._name)])
+        action.update({
+                        'target': 'current',
+                        'context': {
+                                    'search_default_res_id': self.id,
+                                    'search_default_model_id': model_id.id
+                                    }
+                        })
+        return action
