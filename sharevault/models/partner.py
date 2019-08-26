9e92ea067495d30aa1e5cf903438423b9af4fb65
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-08-26 19:14:54 +0100

    Revert "[UPD] new fields"
    This reverts commit 3428da75e2749b584a7ad9d9d4febb86c2bb801d.

--- sharevault/models/partner.py
+++ sharevault/models/partner.py
@@ -107,15 +107,7 @@ class Partner(models.Model):
 
 
     # new fields from XLS (select)
-    job_level = fields.Selection([
-                                ('CLEVEL','CLEVEL'),
-                                ('Dir+','Dir+'),
-                                ('IndContributor','IndContributor'),
-                                ('Mgr+','Mgr+'),
-                                ('Other','Other'),
-                                ('VP+','VP+')
-                                ], 'Job Level')
-
+    job_level = fields.Selection([('null','null')], 'Job Level') #SKYPE
     sharevault_activated_user = fields.Selection([('null','null')], 'ShareVault Activated User')
     sharevault_admin = fields.Selection([('null','null')], 'ShareVault Admin')
     sharevault_domain = fields.Selection([('null','null')], 'ShareVault Domain')
